c2e4aa7e86a90616addba2ded5468445c43489d6
altnum <altnum16@gmail.com>
2021-12-05 06:10:45 +0200

    Added Match and Message models

--- server/src/main/java/me/deit/server/message/Message.java
+++ server/src/main/java/me/deit/server/message/Message.java
@@ -27,4 +27,55 @@ public class Message {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private ZonedDateTime created_at;
+
+    public Message() {
+    }
+
+    public Message(Long id, Match match, User user, String content, ZonedDateTime created_at) {
+        this.id = id;
+        this.match = match;
+        this.user = user;
+        this.content = content;
+        this.created_at = created_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Match getMatch() {
+        return match;
+    }
+
+    public void setMatch(Match match) {
+        this.match = match;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ZonedDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(ZonedDateTime created_at) {
+        this.created_at = created_at;
+    }
 }
