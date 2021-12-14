285ee891abea0641e51988f6025f61f52ac7aa1b
Viktor Naychev <vitonaychev@gmail.com>
2021-12-14 16:55:40 +0200

    Remove unnecessary import

--- server/src/main/java/me/deit/server/browse/BrowseController.java
+++ server/src/main/java/me/deit/server/browse/BrowseController.java
@@ -2,7 +2,6 @@ package me.deit.server.browse;
 
 import me.deit.server.user.User;
 import me.deit.server.user.UserRepository;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
