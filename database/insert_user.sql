72ac99d6521349fa84e95479f90899f8ffb90d2d
Viktor Naychev <vitonaychev@gmail.com>
2021-12-19 20:38:50 +0200

    Add user SQL

---
+++ database/insert_user.sql
@@ -0,0 +1,62 @@
+INSERT INTO public.user (email, password, first_name, last_name, phone_number, gender, preference, description)
+	VALUES ('tom.cat@gmail.com', '', 'Tom', 'Cat', '0774774774', 'MALE', 'WOMEN', 'I''m a chill cat, looking for a mouse ;D');
+UPDATE public.user SET password='$2a$10$7yiWHIgzzzSkJqbpf4AIEeVnNRledLaydd9A5jRWMBhphwZPC6NlK' WHERE id = 1; -- password123
+	
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (1, 1);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (1, 5);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (1, 94);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (1, 131);
+
+INSERT INTO public.user (email, password, first_name, last_name, phone_number, gender, preference, description)
+	VALUES ('peterpan@hotmail.com', '', 'Peter', 'Pan', '068684774', 'MALE', 'WOMEN', 'Wanna make some lucky girl fly tonight ;)');
+UPDATE public.user SET password='$2a$10$7yiWHIgzzzSkJqbpf4AIEeVnNRledLaydd9A5jRWMBhphwZPC6NlK' WHERE id = 2; -- password123
+
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (2, 3);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (2, 28);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (2, 33);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (2, 78);
+
+INSERT INTO public.user (email, password, first_name, last_name, phone_number, gender, preference, description)
+	VALUES ('dwayne-johnson@gmail.com', '', 'Dwayne', 'Johnsom', '0434342111', 'MALE', 'WOMEN', 'Some girls call me The Rock - and not without a reason ;p');
+UPDATE public.user SET password='$2a$10$hCR094pik5t7P3YpdFrVFereOgzflJdjVUAyRx9ouTmCVG9Uc8BPq' WHERE id = 3; -- itsAboutDrive
+	
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (3, 17);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (3, 55);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (3, 88);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (3, 111);
+
+INSERT INTO public.user (email, password, first_name, last_name, phone_number, gender, preference, description)
+	VALUES ('mia-khalifa@gmail.com', '', 'Mia', 'Khalifa', '0696969696', 'FEMALE', 'BOTH', 'You know who I am ;*');
+UPDATE public.user SET password='$2a$10$9YohuBt7oYMpoOaeZiunIe5zab9e5bvWoi7rM2OmJpTTSR7NWFnn6' WHERE id = 4; -- XXL
+	
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (4, 28);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (4, 39);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (4, 201);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (4, 243);
+
+INSERT INTO public.user (email, password, first_name, last_name, phone_number, gender, preference, description)
+	VALUES ('tobby@gmail.com', '', 'Toby', 'Maguire', '09912393244', 'MALE', 'WOMEN', 'Gonna cry?');
+UPDATE public.user SET password='$2a$10$7m4R/qHH5gpUDchfVc7SVeWQSawmMElvDZkIbkhGnV3kV.65zteja' WHERE id = 5; -- spider-man
+
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (5, 30);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (5, 31);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (5, 222);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (5, 244);
+
+INSERT INTO public.user (email, password, first_name, last_name, phone_number, gender, preference, description)
+	VALUES ('pete-david@gmail.com', '', 'Pete', 'Davidson', '05554443332', 'MALE', 'WOMEN', 'Profession: Bagging A list women ;D');
+UPDATE public.user SET password='$2a$10$n1/1bDpgvkIfZZCvyATNe.rNONYjnEgFDvKFTlyPZmzKVuVVlihbK' WHERE id = 6; -- kanye_west
+
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (6, 1);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (6, 2);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (6, 3);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (6, 4);
+
+INSERT INTO public.user (email, password, first_name, last_name, phone_number, gender, preference, description)
+	VALUES ('shelby@gmail.com', '', 'Thomas', 'Shelby', '032233212', 'MALE', 'WOMEN', 'Profession: Bagging A list women and men ;D');
+UPDATE public.user SET password='$2a$10$Nr/YQJbZQSKHd0tZjicvo.TVNque5cccmQGWZnirQ84oOvNafpx0W' WHERE id = 7; -- no_fighting
+
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (7, 12);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (7, 23);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (7, 34);
+INSERT INTO public.user_hobby (user_id, hobby_id) VALUES (7, 45);
